39c144eb6e1ffc98abc41ae224094917917acff6
Ata Safapour <safapour.ata@gmail.com>
2023-08-01 04:38:59 +0330

    feat: added sentry config for error tracking

--- trello/settings.py
+++ trello/settings.py
@@ -156,3 +156,19 @@ LOGGING = {
     }
 }
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+  dsn="https://4ccd4883458fb8f5dbcae80f2e748bea@o4505626896367616.ingest.sentry.io/4505626911178752",
+  integrations=[DjangoIntegration()],
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  traces_sample_rate=1.0,
+
+  # If you wish to associate users to errors (assuming you are using
+  # django.contrib.auth) you may enable sending PII data.
+  send_default_pii=True
+)
