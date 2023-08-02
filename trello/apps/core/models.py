edb1fe0a63753fbcdcd596c5777b4ba62bbfb680
Ata Safapour <safapour.ata@gmail.com>
2023-08-02 18:54:10 +0330

    feat: added basemodel in core app

--- trello/apps/core/models.py
+++ trello/apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from uuid import uuid4
 
-# Create your models here.
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
+    create_at = models.DateTimeField(_("Create at"), auto_now=False, auto_now_add=True)
+    update_at = models.DateTimeField(_("Update at"), auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+
+
+
