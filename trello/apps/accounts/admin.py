09aaa8430e130f5db21b7a6181a1bdae480a9b7e
Ata Safapour <safapour.ata@gmail.com>
2023-08-03 02:57:56 +0330

    feat: added custom user to admin panel

--- trello/apps/accounts/admin.py
+++ trello/apps/accounts/admin.py
@@ -1,3 +1,37 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class AppUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "mobile")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "mobile", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("mobile", "first_name", "last_name", "email")
+    ordering = ("email",)
