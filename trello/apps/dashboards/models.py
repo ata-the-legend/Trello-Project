4ace74de470a97a5c5dcfff2af23de6d9074094a
Your Name <sinaahmadi615@gmail.com>
2023-08-02 19:11:09 +0330

    feat: Add Task,Label and Comment data model

--- trello/apps/dashboards/models.py
+++ trello/apps/dashboards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(BaseModel):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    status = models.CharField(max_length=300)
+    order = models.IntegerField()
+    label = models.ForeignKey('Label',on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    assigned_to = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Label(BaseModel):
+    title = models.CharField(max_length=300)
+    board = models.ForeignKey('Board', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(BaseModel):
+    body = models.TextField()
+    task = models.ForeignKey('Task', on_delete=models.CASCADE)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)    
