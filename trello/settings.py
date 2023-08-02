06e1e33796a76544d9917416fb684ca2548abc7d
Ata Safapour <safapour.ata@gmail.com>
2023-08-02 16:43:41 +0330

    build: created core, accounts, dashboards apps

--- trello/settings.py
+++ trello/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+DEVELOPED_APPS = [
+    'trello.apps.core.apps.CoreConfig',
+    'trello.apps.accounts.apps.AccountsConfig',
+    'trello.apps.dashboards.apps.DashboardsConfig',
+]
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS = SYSTEM_APPS + DEVELOPED_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
