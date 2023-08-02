31776e49e9e5daedc9d997fd0a99fe0eccb001a7
Your Name <sinaahmadi615@gmail.com>
2023-08-02 19:44:59 +0330

    fix: fix some errors

--- trello/apps/dashboards/models.py
+++ trello/apps/dashboards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from trello.apps.core.models import BaseModel
 
 
 class Task(BaseModel):
@@ -10,17 +11,14 @@ class Task(BaseModel):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     assigned_to = models.ForeignKey('User', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Label(BaseModel):
     title = models.CharField(max_length=300)
     board = models.ForeignKey('Board', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(BaseModel):
     body = models.TextField()
     task = models.ForeignKey('Task', on_delete=models.CASCADE)
     author = models.ForeignKey('User', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)    
