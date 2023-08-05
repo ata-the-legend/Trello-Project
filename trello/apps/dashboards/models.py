4148471a8b240b1fe20de94ca9dedbd504fe37cc
Your Name <sinaahmadi615@gmail.com>
2023-08-05 10:20:06 +0330

    feat: add meta class and some feature

--- trello/apps/dashboards/models.py
+++ trello/apps/dashboards/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from trello.apps.core.models import BaseModel
+from trello.apps.core.models import BaseModel, SoftDeleteMixin
 from django.utils.translation import gettext as _
 
 
@@ -22,11 +22,17 @@ class List(BaseModel):
 
 
 
-class Label(BaseModel):
+class Label(BaseModel, SoftDeleteMixin):
     title = models.CharField(max_length=300, verbose_name=_('Title'), help_text='Title of the label')
     board = models.ForeignKey(Board, on_delete=models.CASCADE, verbose_name=_('Board'), help_text='Board associated with the label', related_name='board_labels')
+    
+    class Meta:
+        verbose_name = _('Label')
+        verbose_name_plural =_("Labels")
+    def __str__(self):
+        return self.title
 
-class Task(BaseModel):
+class Task(BaseModel, SoftDeleteMixin):
     title = models.CharField(max_length=300, verbose_name=_('Title'), help_text='Title of the task')
     description = models.TextField(verbose_name=_('Description'), help_text='Description of the task')
     status = models.ForeignKey(List ,verbose_name=_('Status'), on_delete=models.CASCADE, help_text='Status of the task', related_name='status_tasks')
@@ -36,14 +42,25 @@ class Task(BaseModel):
     end_date = models.DateTimeField(verbose_name=_('End Date'), help_text='End date of the task', null=True, blank=True)
     assigned_to = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('Assigned To'), help_text='User assigned to the task', related_name='assigned_tasks')
 
-
-class Comment(BaseModel):
+    class Meta:
+        verbose_name =_('Task')
+        verbose_name_plural =_('Tasks')
+    def __str__(self):
+        return f'Task {self.title}'
+    
+class Comment(BaseModel, SoftDeleteMixin):
     body = models.TextField(verbose_name=_('Body'), help_text='Body of the comment')
     task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name=_('Task'), help_text='Task associated with the comment', related_name='task_comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, verbose_name=_('Author'), help_text='Author of the comment', related_name='author_comments')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name=_('Parent'), null=True, blank=True)
 
 
+    class Meta:
+        verbose_name = _('Comment')
+        verbose_name_plural =_('Comments')
+
+    def __str__(self):
+        return f'Comment by {self.author} on task {self.task}'
 
 class Attachment(BaseModel):
     file = models.FileField(verbose_name=_("file") , max_length=100 ,upload_to='attachments/')
