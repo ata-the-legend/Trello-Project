5b1c46e555605f6231471c58e3f533441cb227a2
mahshid <mahshidvafei08@gmail.com>
2023-08-12 17:17:10 +0330

    add tests

--- trello/apps/dashboards/tests.py
+++ trello/apps/dashboards/tests.py
@@ -2,81 +2,213 @@ from django.test import TestCase
 from trello.apps.dashboards.models import Label, Board, Task, Activity,TaskList,Comment
 from trello.apps.accounts.models import User
 
-class LabelTestCase(TestCase):
-    def setUp(self):
-        self.board = Board.objects.create(title="Test Board")
-        self.task = Task.objects.create(title="Test Task", status=self.board)
-        self.user = User.objects.create(email="test@example.com", password="password")
+# class LabelTestCase(TestCase):
+#     def setUp(self):
+#         self.board = Board.objects.create(title="Test Board")
+#         self.task = Task.objects.create(title="Test Task", status=self.board)
+#         self.user = User.objects.create(email="test@example.com", password="password")
 
-    def test_create_label(self):
-        label = Label.create_label("Test Label", self.board, self.task, self.user)
-        self.assertEqual(label.title, "Test Label")
-        self.assertEqual(label.board, self.board)
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} created a new label Test Label on task {self.task.title}.")
+#     def test_create_label(self):
+#         label = Label.create_label("Test Label", self.board, self.task, self.user)
+#         self.assertEqual(label.title, "Test Label")
+#         self.assertEqual(label.board, self.board)
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} created a new label Test Label on task {self.task.title}.")
 
-    def test_delete_label(self):
-        label = Label.create_label("Test Label", self.board, self.task, self.user)
-        label.delete(self.task, self.user)
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} deleted the label Test Label on task {self.task.title}.")
+#     def test_delete_label(self):
+#         label = Label.create_label("Test Label", self.board, self.task, self.user)
+#         label.delete(self.task, self.user)
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} deleted the label Test Label on task {self.task.title}.")
 
-    def test_update_label(self):
-        label = Label.create_label("Test Label", self.board, self.task, self.user)
-        label.update_label("Updated Label", self.task, self.user)
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} updated the label Updated Label on task {self.task.title}.")
+#     def test_update_label(self):
+#         label = Label.create_label("Test Label", self.board, self.task, self.user)
+#         label.update_label("Updated Label", self.task, self.user)
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} updated the label Updated Label on task {self.task.title}.")
 
-class TaskTestCase(TestCase):
-    def setUp(self):
-        self.tasklist = TaskList.objects.create(title="Test TaskList")
-        self.user = User.objects.create(email="test@example.com", password="password")
-        self.label = Label.objects.create(title="Test Label", board=self.tasklist.board)
+# class TaskTestCase(TestCase):
+#     def setUp(self):
+#         self.tasklist = TaskList.objects.create(title="Test TaskList")
+#         self.user = User.objects.create(email="test@example.com", password="password")
+#         self.label = Label.objects.create(title="Test Label", board=self.tasklist.board)
 
-    def test_create_task(self):
-        task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
-        self.assertEqual(task.title, "Test Task")
-        self.assertEqual(task.description, "Test Description")
-        self.assertEqual(task.status, self.tasklist)
-        self.assertEqual(task.order, 1)
-        self.assertListEqual(list(task.labels.all()), [self.label])
-        self.assertListEqual(list(task.assigned_to.all()), [self.user])
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"A new task Test Task was created.")
+#     def test_create_task(self):
+#         task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
+#         self.assertEqual(task.title, "Test Task")
+#         self.assertEqual(task.description, "Test Description")
+#         self.assertEqual(task.status, self.tasklist)
+#         self.assertEqual(task.order, 1)
+#         self.assertListEqual(list(task.labels.all()), [self.label])
+#         self.assertListEqual(list(task.assigned_to.all()), [self.user])
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"A new task Test Task was created.")
 
-    def test_update_task(self):
-        task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
-        task.update_task("Updated Task", "Updated Description", order=2)
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"Task Updated Task was updated.")
+#     def test_update_task(self):
+#         task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
+#         task.update_task("Updated Task", "Updated Description", order=2)
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"Task Updated Task was updated.")
 
-    def test_delete_task(self):
-        task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
-        task.delete()
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"Task Test Task was deleted.")
+#     def test_delete_task(self):
+#         task = Task.create_task("Test Task", "Test Description", self.tasklist, 1, [self.label], assigned_to=[self.user])
+#         task.delete()
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"Task Test Task was deleted.")
 
-class CommentTestCase(TestCase):
-    def setUp(self):
-        self.task = Task.objects.create(title="Test Task")
-        self.user = User.objects.create(email="test@example.com", password="password")
+# class CommentTestCase(TestCase):
+#     def setUp(self):
+#         self.task = Task.objects.create(title="Test Task")
+#         self.user = User.objects.create(email="test@example.com", password="password")
 
-    def test_create_comment(self):
-        comment = Comment.create_comment("Test Comment", self.task, self.user)
-        self.assertEqual(comment.body, "Test Comment")
-        self.assertEqual(comment.task, self.task)
-        self.assertEqual(comment.author, self.user)
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} added a new comment to task {self.task.title}.")
+#     def test_create_comment(self):
+#         comment = Comment.create_comment("Test Comment", self.task, self.user)
+#         self.assertEqual(comment.body, "Test Comment")
+#         self.assertEqual(comment.task, self.task)
+#         self.assertEqual(comment.author, self.user)
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} added a new comment to task {self.task.title}.")
 
-    def test_update_comment(self):
-        comment = Comment.create_comment("Test Comment", self.task, self.user)
-        comment.update_comment("Updated Comment")
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} updated a comment on task {self.task.title}.")
+#     def test_update_comment(self):
+#         comment = Comment.create_comment("Test Comment", self.task, self.user)
+#         comment.update_comment("Updated Comment")
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} updated a comment on task {self.task.title}.")
 
-    def test_delete_comment(self):
-        comment = Comment.create_comment("Test Comment", self.task, self.user)
-        comment.delete()
-        activity = Activity.objects.last()
-        self.assertEqual(activity.message, f"{self.user.get_full_name()} deleted a comment on task {self.task.title}.")
+#     def test_delete_comment(self):
+#         comment = Comment.create_comment("Test Comment", self.task, self.user)
+#         comment.delete()
+#         activity = Activity.objects.last()
+#         self.assertEqual(activity.message, f"{self.user.get_full_name()} deleted a comment on task {self.task.title}.")
+
+
+class WorkSpaceTestCase(TestCase):
+    
+    def setUp(self) -> None:
+        self.user_ata = User.objects.create_user(email='ata@gmail.COM', password='1234', first_name='ata', last_name='test', avatar=None, mobile='01234567890')
+        self.user_ali = User.objects.create_user(email='ali@gmail.COM', password='1234', first_name='ali', last_name='test', mobile='01234567893')
+        self.workspace_ata = WorkSpace.objects.create(title='Ata Workspace', owner=self.user_ata)
+        self.workspace_ata.members.add(self.user_ali)
+        self.workspace_ata_2 = WorkSpace.objects.create(title='New Workspace', owner=self.user_ata)
+        self.board_ata_1_1 = Board.objects.create(title='Board One', work_space=self.workspace_ata)
+        self.list_1_1_1 = TaskList.objects.create(title='List One', board= self.board_ata_1_1)
+        self.task_1 = Task.objects.create(title='Task One', description='...', status=self.list_1_1_1, order=1)
+       
+        return super().setUp()
+
+    def test_work_space(self):
+        self.assertTrue(WorkSpace.objects.filter(title=self.workspace_ata.title, owner=self.workspace_ata.owner).exists())
+
+    def test_add_member(self):
+        w_member = self.workspace_ata.add_member(self.user_ali)
+        self.assertEqual(w_member, self.workspace_ata)
+
+    def test_add_board(self):
+        w_board = self.workspace_ata.add_board(title=self.board_ata_1_1.title, background_image=None)
+        self.assertIs([w_board], [self.board_ata_1_1])
+
+    def test_work_space_members(self):
+        self.assertEqual(self.workspace_ata.work_space_members(), (self.workspace_ata.members.all(), self.user_ata))
+
+    def test_archive(self):
+        self.assertTrue(self.workspace_ata.is_active)
+        self.assertTrue(self.board_ata_1_1.is_active)
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
+
+        self.workspace_ata.archive()
+        self.assertFalse(self.workspace_ata.is_active)
+        self.assertFalse(self.board_ata_1_1.is_active)
+        self.assertFalse(self.list_1_1_1.is_active)
+        self.assertFalse(self.task_1.is_active)
+    
+    def test_restore(self):
+        self.workspace_ata.archive()
+        self.assertFalse(self.workspace_ata.is_active)
+        # self.assertFalse(self.board_ata_1_1.is_active)
+        # self.assertFalse(self.list_1_1_1.is_active)
+        # self.assertFalse(self.task_1.is_active)
+
+        self.workspace_ata.restore()
+        self.assertTrue(self.workspace_ata.is_active)
+        self.assertTrue(self.board_ata_1_1.is_active)
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
+
+class BoardTestCase(TestCase):
+
+    def setUp(self) -> None:
+
+        self.user_ata = User.objects.create_user(email='ata@gmail.COM', password='1234', first_name='ata', last_name='test', avatar=None, mobile='01234567890')        
+        self.workspace_ata = WorkSpace.objects.create(title='Ata Workspace', owner=self.user_ata)
+        self.board_ata_1_1 = Board.objects.create(title='Board One', work_space=self.workspace_ata)
+        self.list_1_1_1 = TaskList.objects.create(title='List One', board= self.board_ata_1_1)
+        self.task_1 = Task.objects.create(title='Task One', description='...', status=self.list_1_1_1, order=1)
+        self.label_1 = Label.objects.create(title='Label one', board=self.board_ata_1_1)
+        
+        return super().setUp()
+
+    def test_add_tasklist(self):
+        b_tasklist = self.board_ata_1_1.add_tasklist(title=self.list_1_1_1.title)
+        self.assertEqual([b_tasklist], [self.list_1_1_1])
+
+    def test_get_board_labels(self):
+        board_labels = self.board_ata_1_1.get_board_labels()
+        self.assertCountEqual(board_labels, self.board_ata_1_1.board_labels.all())
+
+    def test_archive(self):
+        self.assertTrue(self.board_ata_1_1.is_active)
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
+
+        self.board_ata_1_1.archive()
+        self.assertFalse(self.board_ata_1_1.is_active)
+        self.assertFalse(self.list_1_1_1.is_active)
+        self.assertFalse(self.task_1.is_active)
+    
+    def test_restore(self):
+        self.board_ata_1_1.archive()
+        # self.assertFalse(self.board_ata_1_1.is_active)
+        # self.assertFalse(self.list_1_1_1.is_active)
+        # self.assertFalse(self.task_1.is_active)
+
+        self.board_ata_1_1.restore()
+        self.assertTrue(self.board_ata_1_1.is_active)
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
+   
+class TaskListTestCase(TestCase):
+
+    def setUp(self) -> None:
+
+        self.user_ata = User.objects.create_user(email='ata@gmail.COM', password='1234', first_name='ata', last_name='test', avatar=None, mobile='01234567890')        
+        self.workspace_ata = WorkSpace.objects.create(title='Ata Workspace', owner=self.user_ata)
+        self.board_ata_1_1 = Board.objects.create(title='Board One', work_space=self.workspace_ata)
+        self.list_1_1_1 = TaskList.objects.create(title='List One', board= self.board_ata_1_1)
+        self.task_1 = Task.objects.create(title='Task One', description='...', status=self.list_1_1_1, order=1)
+        
+        return super().setUp()
+
+    def test_add_task(self):
+        l_task = self.list_1_1_1.add_task(title='Task One', description='...', assigned_to=self.user_ata)
+        self.assertEqual(l_task, self.list_1_1_1)
+        
+    def test_task_count(self):
+        self.assertEqual(self.list_1_1_1.task_count(), self.list_1_1_1.status_tasks.all().count())
+
+    def test_archive(self):
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
+
+        self.list_1_1_1.archive()
+        self.assertFalse(self.list_1_1_1.is_active)
+        self.assertFalse(self.task_1.is_active)
+    
+    def test_restore(self):
+        self.list_1_1_1.archive()
+        # self.assertFalse(self.list_1_1_1.is_active)
+        # self.assertFalse(self.task_1.is_active)
+
+        self.list_1_1_1.restore()
+        self.assertTrue(self.list_1_1_1.is_active)
+        self.assertTrue(self.task_1.is_active)
