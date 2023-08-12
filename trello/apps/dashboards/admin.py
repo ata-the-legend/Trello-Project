4cff5a29a4c5ab312891b23224b77b22cc430344
Your Name <sinaahmadi615@gmail.com>
2023-08-12 11:24:29 +0330

    feat: add Taskadmin

--- trello/apps/dashboards/admin.py
+++ trello/apps/dashboards/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _ 
+from .models import Task, Comment, Label
 
-# Register your models here.
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields':('title','description', 'status', 'order', 'labels','start_date', 'end_date', 'assigned_to')}),(_('Permissions'),{'fields':('is_active',)}),
+    )
+    inlines = [CommentInLine]
+    list_display = ('title','status','order','start_date','end_date')
+    list_filter = ('status', 'labels','assigned_to')
+    search_fields = ('title',)
+    ordering = ('status', 'order')
+    
+
