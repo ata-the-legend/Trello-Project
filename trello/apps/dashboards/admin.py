e6249520f747032a17d29cdf47f6e72a7818f5dd
ho3infj <farajihossein1379@gmail.com>
2023-08-14 20:49:57 +0330

    Added attachment and activity fields to the admin panel

--- trello/apps/dashboards/admin.py
+++ trello/apps/dashboards/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Attachment , Activity
 
-# Register your models here.
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('file', 'task', 'owner')}),
+    )
+    list_display = ('file', 'task', 'owner')
+    list_filter = ('task',)
+    search_fields = ('file',)
+    ordering = ('task',)
+
+
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('doer', 'message', 'task')}),
+    )
+    list_display = ('doer', 'message', 'task')
+    list_filter = ('task',)
+    search_fields = ('message',)
+    ordering = ('task',)
