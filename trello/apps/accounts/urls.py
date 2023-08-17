1ee6e7d2267fc160d1df22d934722323f901c45e
Ata Safapour <safapour.ata@gmail.com>
2023-08-17 04:35:05 +0330

    build: install and config simplejwt

--- trello/apps/accounts/urls.py
+++ trello/apps/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .routers import UserCustomRouter
 from .views import UserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 app_name = 'accounts'
-urlpatterns = []
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
 
 router = UserCustomRouter()
--- trello/settings.py
+++ trello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,20 @@ THIRD_PARTY_APPS = [
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + DEVELOPED_APPS + THIRD_PARTY_APPS
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
 
 AUTH_USER_MODEL = "accounts.User"
 
