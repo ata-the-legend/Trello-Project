d9cdb4f9c49e2ee9a14b00daef28dee43126413d
Ata Safapour <safapour.ata@gmail.com>
2023-08-22 06:09:18 +0330

    feat: get all objs in WorkspaceAdmin qs

--- trello/apps/dashboards/admin.py
+++ trello/apps/dashboards/admin.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import WorkSpace, Board, Attachment , Activity
 from .forms import MembersM2MAdminForm
 
@@ -13,8 +16,17 @@ class MembersInline(admin.TabularInline):
 @admin.register(WorkSpace)
 class WorkspaceAdmin(admin.ModelAdmin):
     list_display = ['title', 'owner']
-    fields = ['title', 'owner']
+    fields = ['title', 'owner', 'is_active']
     inlines = (MembersInline,)
+    list_filter = ['is_active']
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        qs = self.model.original_objects.get_queryset()
+        # TODO: this should be handled by some parameter to the ChangeList.
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
 
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
@@ -30,6 +42,14 @@ class AttachmentAdmin(admin.ModelAdmin):
     list_filter = ('is_active',)
     search_fields = ('task','owner')
     ordering = ('task',)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        qs = self.model.original_objects.get_queryset()
+        # TODO: this should be handled by some parameter to the ChangeList.
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
 
 
 @admin.register(Activity)
