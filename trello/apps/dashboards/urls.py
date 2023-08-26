04c895c7cde231fe1bd00560e84f445fe00b40cd
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-27 01:39:09 +0330

    feat:add urls

--- trello/apps/dashboards/urls.py
+++ trello/apps/dashboards/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
+from trello.apps.dashboards.views.attachment_views import AttachmentViewSet
 
 app_name = 'dashboards'
 urlpatterns = []
 
 router = routers.DefaultRouter()
-# router.register(prefix='' ,viewset=...ViewSet)
+router.register(prefix='attachment' ,viewset=AttachmentViewSet)
 # router.register(prefix='' ,viewset=...ViewSet)
 # router.register(prefix='' ,viewset=...ViewSet)
 
