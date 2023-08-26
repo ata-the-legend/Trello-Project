588b730fca2e5a4d426aae1158fe4ad61c4668e0
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-27 00:55:09 +0330

    feat:fix label api

--- trello/apps/dashboards/serializers.py
+++ trello/apps/dashboards/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
-from trello.apps.dashboards.models import Label, Task
+from trello.apps.dashboards.models import Label, Task,Board
 
-class LabelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Label
-        fields = ('__all__')
+class LabelSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=300)
+    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
+
+    def create(self, validated_data):
+        return Label.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
+        instance.board = validated_data.get('board', instance.board)
         instance.save()
         return instance
-
-
+    
 class TaskSerializer(serializers.ModelSerializer):
     
     class Meta:
