c0f46264f5acf53a1af3047c183d862d9a33aa08
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-26 18:08:40 +0330

    feat: add LabelViewSet

---
+++ trello/apps/dashboards/views/label_views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from dashboards.serializers import LabelSerializer
+from trello.apps.dashboards.models import Label
+
+
+class LabelViewSet(viewsets.ModelViewSet):
+    queryset = Label.objects.all()
+    serializer_class = LabelSerializer
+    
+    def create_label(self, request):
+        title = request.data.get('title')
+        board = request.data.get('board')
+        label = Label.create_label(title, board)
+        serializer = self.get_serializer(label)
+        return Response(serializer.data)
+    
+    def get_label_choices(self, request):
+        choices = Label.get_label_choices()
+        return Response(choices)
+    
+    def get_tasks(self, request, pk=None):
+        label = self.get_object()
+        tasks = label.get_tasks()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    def get_task_count(self, request, pk=None):
+        label = self.get_object()
+        count = label.get_task_count()
+        return Response({'count': count})
