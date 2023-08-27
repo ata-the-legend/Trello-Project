8e287455d7d1d8eee7d4f8213f33ccd2bc4e9e5d
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-27 22:55:05 +0330

    feat: add urls

--- trello/apps/dashboards/urls.py
+++ trello/apps/dashboards/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
+from trello.apps.dashboards.views.comment_views import CommentViewSet
 
 app_name = 'dashboards'
 urlpatterns = []
 
 router = routers.DefaultRouter()
-# router.register(prefix='' ,viewset=...ViewSet)
+router.register(prefix='comments' ,viewset=CommentViewSet )
 # router.register(prefix='' ,viewset=...ViewSet)
 # router.register(prefix='' ,viewset=...ViewSet)
 
