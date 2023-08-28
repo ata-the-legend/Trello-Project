6d50ba938197a0db21f340dd56fc9645f4c8b61c
Ata Safapour <safapour.ata@gmail.com>
2023-08-28 05:49:47 +0330

    feat: added workspace viewsets

--- trello/apps/dashboards/urls.py
+++ trello/apps/dashboards/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
+from trello.apps.dashboards.views import workspace_views 
 
 app_name = 'dashboards'
 urlpatterns = []
 
 router = routers.DefaultRouter()
 # router.register(prefix='' ,viewset=...ViewSet)
-# router.register(prefix='' ,viewset=...ViewSet)
+router.register(prefix='workspaces' ,viewset=workspace_views.WorkspaceViewSet)
 # router.register(prefix='' ,viewset=...ViewSet)
 
 urlpatterns += router.urls
--- trello/apps/dashboards/views/workspace_views.py
+++ trello/apps/dashboards/views/workspace_views.py
@@ -1,56 +1,39 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import mixins, GenericViewSet
+from ..serializers import WorkspaceAddMemberSerializer, WorkspaceSerializer
 from ..models import WorkSpace
-from ..serializers import WorkSpaceSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from trello.apps.accounts.permissions import UserPermission
-from django.shortcuts import get_object_or_404
+from rest_framework import status
+from drf_spectacular.utils import extend_schema
 
-class WorkSpaceViewSet(viewsets.ViewSet):
-    permission_classes = [UserPermission]
-    serializer_class = WorkSpaceSerializer
-    queryset = WorkSpace.objects.all()
 
-    def list(self, request):
-        serz_data = WorkSpaceSerializer(instance=self.queryset, many=True)
-        return Response(data=serz_data.data)
 
-    def create(self, request):
-        srz_data = WorkSpaceSerializer(data=request.Post)
-        if serz_data.is_valid():
-            WorkSpace.objects.create(
-                title=srz_data.validated_data['title'],
-                members=srz_data.validated_data['members'],
-                owner=srz_data.validated_data['owner'],
-            )
+class WorkspaceViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
+    
+    queryset = WorkSpace.objects.all().prefetch_related('work_space_boards')
+    serializer_class = WorkspaceSerializer
 
-    def retrieve(self, request, pk=None):
-        workspace = get_object_or_404(self.queryset, pk=pk)
-        serz_data = WorkSpaceSerializer(instance=workspace)
-        return Response(data=serz_data.data)
+    def perform_destroy(self, instance):
+        return instance.archive()
 
-    def update(self, request, pk=None):
-        workspace = get_object_or_404(self.queryset, pk=pk)
-        if workspace.owner != request.user:
-            return Response({'permission denied':'you are not the owner.'})
-        serz_data = WorkSpaceSerializer(instance=workspace, data=request.Post)
-        if serz_data.is_valid():
-            serz_data.save()
-            return Response(data=serz_data.data)
-        return Response(data=serz_data.errors)
+    @extend_schema(request=WorkspaceAddMemberSerializer, responses=WorkspaceAddMemberSerializer)
+    @action(methods=['post'],detail=True, url_path='add-members')
+    def add_members(self, request, *args, **kwargs):
+        workspace = self.get_object()
+        serializer = WorkspaceAddMemberSerializer(data=request.data)
+        if serializer.is_valid():
+            for member in serializer.validated_data['new_members']:
+                if request.user == member:
+                    continue
+                workspace.add_member(member)
+            workspace.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    def partial_update(self, request, pk=None):
-        workspace = get_object_or_404(self.queryset, pk=pk)
-        if workspace.owner != request.user:
-            return Response({'permission denied':'you are not the owner.'})
-        serz_data = WorkSpaceSerializer(instance=workspace, data=request.Post, partial=True)
-        if serz_data.is_valid():
-            serz_data.save()
-            return Response(data=serz_data.data)
-        return Response(data=serz_data.errors)
 
-    def destroy(self, request, pk=None):
-        workspace = get_object_or_404(self.queryset, pk=pk)
-        workspace.archive()
-        workspace.save()
-        return Response({"message":"work space deactivated."})
