03f106beb1ff7d3c4279377e40caaa27c10beb4c
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-29 10:51:17 +0330

    feat: add permission to LabelSerializer

--- trello/apps/dashboards/views/label_views.py
+++ trello/apps/dashboards/views/label_views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets ,status
 from rest_framework.response import Response
 from trello.apps.dashboards.serializers import LabelSerializer, TaskSerializer
 from trello.apps.dashboards.models import Label
+from rest_framework import permissions
+from trello.apps.dashboards.permissions import LabelPermission
 
 
 class LabelViewSet(viewsets.ModelViewSet):
  
     serializer_class = LabelSerializer
     queryset = Label.objects.all()
+    permission_classes = [permissions.IsAuthenticated, LabelPermission]
