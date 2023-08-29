ec9f35db6a0afdcd8ab3cdf43708063382cae788
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-29 22:53:40 +0330

    fix bugs'
    c

--- trello/apps/dashboards/serializers.py
+++ trello/apps/dashboards/serializers.py
@@ -5,29 +5,30 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for creating and updating comments.
     """
-    parent = serializers.PrimaryKeyRelatedField(queryset=Comment.objects.all(), required=False)
+    
    
 
 
     class Meta:
         model = Comment
-        fields = ['id', 'body', 'task', 'author', 'parent']
-        read_only_fields = ['id', 'task', 'author']
+        exclude =('is_active',)
+
 
     def create(self, validated_data):
         """
         Create a new comment instance.
         """
-        task = self.context.get('task') 
-        author = self.context['request'].user
-        parent = validated_data.get('parent')
-        comment = Comment.create_comment(body=validated_data['body'], task=task, author=author, parent=parent)
+        author = self.context['request'].user 
+        task = validated_data['task']
+        body = validated_data['body']
+        parent = validated_data.get('parent', None)
+        comment = Comment.create_comment(body=body, task=task, author=author, parent=parent)
         return comment
 
     def update(self, instance, validated_data):
         """
         Update an existing comment instance.
         """
-        body = validated_data.get('body')
-        instance.update_comment(body)
+        body = validated_data.get('body', instance.body)
+        instance.update_comment(body=body)
         return instance
--- trello/apps/dashboards/views/comment_views.py
+++ trello/apps/dashboards/views/comment_views.py
@@ -7,21 +7,16 @@ from rest_framework import permissions
 from trello.apps.dashboards.permissions import CommentPermission
 
 
-class SoftDestroyModelMixin:
-    """
-    Mixin for soft-deleting model instances
-    """
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.archive()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class SoftDestroyModelMixin:
+#     """
+#     Mixin for soft-deleting model instances
+#     """
+#     def destroy(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         instance.archive()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
     
-class CommentViewSet(mixins.CreateModelMixin,
-                   mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   SoftDestroyModelMixin,
-                   mixins.ListModelMixin,
-                   viewsets.GenericViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     """
     This viewset allows creating, retrieving, updating, soft-deleting, and listing comments.
     Soft-deletion is performed using the 'SoftDestroyModelMixin' to archive comments
@@ -29,6 +24,7 @@ class CommentViewSet(mixins.CreateModelMixin,
     """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, CommentPermission]
+    # permission_classes = [permissions.IsAuthenticated, CommentPermission]
 
-
+    def perform_destroy(self, instance):
+        return instance.archive()
