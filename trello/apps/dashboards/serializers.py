290de771c45d4b53d9b71d5bbd36d6676dea1f2b
Ata Safapour <safapour.ata@gmail.com>
2023-08-29 04:35:44 +0330

    feat: added TaskSerializer

--- trello/apps/dashboards/serializers.py
+++ trello/apps/dashboards/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from trello.apps.accounts.models import User
-from .models import Board, WorkSpace
+from .models import Activity, Attachment, Board, Comment, Label, Task, TaskList, WorkSpace
+import traceback
+from rest_framework.utils import model_meta
 
 class UserListSerializer(serializers.ModelSerializer):
 
@@ -18,7 +20,11 @@ class WorkspaceSerializer(serializers.ModelSerializer):
     work_space_boards = BoardListSerializer(many=True, read_only=True)
     owner = UserListSerializer(read_only=True)
     members = UserListSerializer(many=True, read_only=True)
-    add_members = serializers.PrimaryKeyRelatedField(queryset=User.objects.all().exclude(id=owner.data.get(id)), many=True, write_only=True)
+    add_members = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), 
+        many=True, 
+        write_only=True
+    )
 
     class Meta:
         model = WorkSpace
@@ -57,3 +63,130 @@ class WorkspaceSerializer(serializers.ModelSerializer):
 class WorkspaceAddMemberSerializer(serializers.Serializer):
     new_members = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
 
+
+class TaskListListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskList
+        fields = ["id", 'title']
+
+class LabelListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ["id", 'title']
+
+class CommentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        exclude = ['is_active']
+
+class AttachmentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        exclude = ['is_active']
+
+class ActivityListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    obj_status = TaskListListSerializer(read_only=True, source='status')
+    obj_labels = LabelListSerializer(many=True, read_only=True, source='labels')
+    labels=serializers.PrimaryKeyRelatedField(queryset=Label.objects.all(), many=True, write_only=True)
+    obj_assigned_to = UserListSerializer(many=True, read_only=True, source='assigned_to')
+    assigned_to = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, write_only=True)
+    task_comments = CommentListSerializer(many=True, read_only=True)
+    task_attachments = AttachmentListSerializer(many=True, read_only=True)
+    task_activity = ActivityListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        exclude = ['is_active']
+        read_only_fields = ['update_at', 'create_at']
+        extra_kwargs = {
+            'description': {'required': False},
+            'status': {'write_only':True},
+        }
+
+
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+        
+    def create(self, validated_data):
+        """
+        We have a bit of extra checking around this in order to provide
+        descriptive messages when something goes wrong, but this method is
+        essentially just:
+
+            return ExampleModel.objects.create(**validated_data)
+
+        If there are many to many fields present on the instance then they
+        cannot be set until the model is instantiated, in which case the
+        implementation is like so:
+
+            example_relationship = validated_data.pop('example_relationship')
+            instance = ExampleModel.objects.create(**validated_data)
+            instance.example_relationship = example_relationship
+            return instance
+
+        The default implementation also does not handle nested relationships.
+        If you want to support writable nested relationships you'll need
+        to write an explicit `.create()` method.
+        """
+        serializers.raise_errors_on_nested_writes('create', self, validated_data)
+
+        ModelClass = self.Meta.model
+        doer = self._user()
+
+        # Remove many-to-many relationships from validated_data.
+        # They are not valid arguments to the default `.create()` method,
+        # as they require that the instance has already been saved.
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+
+        try:
+            instance = ModelClass.create_task(**validated_data, doer=doer)
+        except TypeError:
+            tb = traceback.format_exc()
+            msg = (
+                'Got a `TypeError` when calling `%s.%s.create()`. '
+                'This may be because you have a writable field on the '
+                'serializer class that is not a valid argument to '
+                '`%s.%s.create()`. You may need to make the field '
+                'read-only, or override the %s.create() method to handle '
+                'this correctly.\nOriginal exception was:\n %s' %
+                (
+                    ModelClass.__name__,
+                    ModelClass._default_manager.name,
+                    ModelClass.__name__,
+                    ModelClass._default_manager.name,
+                    self.__class__.__name__,
+                    tb
+                )
+            )
+            raise TypeError(msg)
+
+        # Save many-to-many relationships after the instance is created.
+        if many_to_many:
+            for field_name, value in many_to_many.items():
+                field = getattr(instance, field_name)
+                field.set(value)
+
+        return instance
+
+    def update(self, instance, validated_data):
+        serializers.raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(instance)
+        doer = self._user()
+
+        instance.update_task(doer=doer, **validated_data)
+
+        return instance
+    
