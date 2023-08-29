8b2bf37e52421219df264e2f7445c000614375d0
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-08-29 10:49:16 +0330

    update permission policy

--- trello/apps/dashboards/permissions.py
+++ trello/apps/dashboards/permissions.py
@@ -5,11 +5,13 @@ from trello.apps.dashboards.models import *
 
 class LabelPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action in ['create', 'update', 'destroy']:
-            user = request.user
+        user = request.user
+        if view.action in ['list', 'retrieve']:
+            label = view.get_object()
+            workspace_members = label.board.work_space.members.all()
+            return user in workspace_members
+        elif view.action in ['create', 'update', 'destroy']:
             label = view.get_object()
             workspace_owner = label.board.work_space.owner
             return user == label.owner or user == workspace_owner
         return True
-
-
