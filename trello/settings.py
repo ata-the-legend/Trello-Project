80040b96bc8ae4ecebad221a6988ae5f1d347e31
Ata Safapour <safapour.ata@gmail.com>
2023-08-30 08:22:36 +0330

    feat: connect project to postgres

--- trello/settings.py
+++ trello/settings.py
@@ -120,10 +120,14 @@ WSGI_APPLICATION = 'trello.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'trello',
+       'USER': 'ata',
+       'PASSWORD': '1234',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
